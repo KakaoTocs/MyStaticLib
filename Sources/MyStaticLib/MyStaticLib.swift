9cfd3f2914fd32c38863c8b098395b5808730503
kimjinu <kimjinu5555@gmail.com>
2021-05-11 16:16:08 +0900

    Add MJTStaticLibrary

--- Sources/MyStaticLib/MyStaticLib.swift
+++ Sources/MyStaticLib/MyStaticLib.swift
@@ -1,3 +1,13 @@
 struct MyStaticLib {
     var text = "Hello, World!"
 }
+
+public class MJTStaticLibrary {
+    public let shared = MJTStaticLibrary()
+    
+    private init() {}
+    
+    public func log() {
+        print(MyStaticLib().text)
+    }
+}
